0b681d53973072412dab19c1de5465c14e57cb0f
vdoubleu <victorwang2001@gmail.com>
2023-09-01 12:24:42 -0400

    testing: alert displaymode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import PageManager from './components/pages/PageManager';
 
 function App() {
+function getPWADisplayMode() {
+  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+  if (document.referrer.startsWith('android-app://')) {
+    return 'twa';
+  } else if (navigator.standalone || isStandalone) {
+    return 'standalone';
+  }
+  return 'browser';
+}
+
+const displayMode = getPWADisplayMode();
+alert(displayMode);
+
     return (
         <>
             <PageManager />
