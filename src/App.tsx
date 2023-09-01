e13ecefea8dc3d5232273f659a2be645b1679815
vdoubleu <victorwang2001@gmail.com>
2023-09-01 12:34:51 -0400

    prompt install

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
+import { useState, useEffect } from 'react';
 import PageManager from './components/pages/PageManager';
 
 function App() {
-function getPWADisplayMode() {
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-  if (document.referrer.startsWith('android-app://')) {
-    return 'twa';
     // @ts-ignore
-  } else if (navigator.standalone || isStandalone) {
-    return 'standalone';
-  }
-  return 'browser';
-}
+    const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+    const [isInstallPromotionVisible, setIsInstallPromotionVisible] = useState(false);
 
-const displayMode = getPWADisplayMode();
-alert(displayMode);
+    useEffect(() => {
+        const displayMode = getPWADisplayMode();
+        console.log(displayMode);
+
+        if (displayMode === 'browser') {
+            window.addEventListener('beforeinstallprompt', (e) => {
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                setDeferredPrompt(e);
+                // Update UI notify the user they can install the PWA
+                setIsInstallPromotionVisible(true);
+                // Optionally, send analytics event that PWA install promo was shown.
+                console.log(`'beforeinstallprompt' event was fired.`);
+            });
+        }
+    }, []);
+
+    function getPWADisplayMode() {
+      const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+      if (document.referrer.startsWith('android-app://')) {
+        return 'twa';
+        // @ts-ignore
+      } else if (navigator.standalone || isStandalone) {
+        return 'standalone';
+      }
+      return 'browser';
+    }
+
+    if (isInstallPromotionVisible) {
+        return (
+            <div> install promotion </div>
+        );
+    }
+                
+
 
     return (
         <>
